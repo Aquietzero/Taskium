29db9a8eadf09bf87e2872e24e0bca10caa93a84
Yunhao Zhao <zhaoyunhaosss@gmail.com>
2012-05-17 17:32:21 +0800

    add: user model

---
+++ taskium/db/migrate/20120517085743_create_users.rb
@@ -0,0 +1,14 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :hashed_password
+      t.string :salt
+      t.string :student_id
+      t.string :email
+      t.string :website
+
+      t.timestamps
+    end
+  end
+end
