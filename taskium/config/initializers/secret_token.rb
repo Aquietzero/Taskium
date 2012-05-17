de1565ff9392260994af13193e117dc981aff0e3
Yunhao Zhao <zhaoyunhaosss@gmail.com>
2012-05-17 16:43:27 +0800

    add: taskium

---
+++ taskium/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Taskium::Application.config.secret_token = '765ac63e155c3ed5ba9d8d7756ddea6709a3304f66eb98f9964f977f041e0de585e4b29037ea056a2243d7886315e0bed8ed6dd6ffdb77499f6e437e3cd1d02a'
