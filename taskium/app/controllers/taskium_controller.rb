b394c387737b002d3ca0e03416b2c933d10c4cd0
Yunhao Zhao <zhaoyunhaosss@gmail.com>
2012-05-18 11:00:52 +0800

    mod: new and update form of post

--- taskium/app/controllers/taskium_controller.rb
+++ taskium/app/controllers/taskium_controller.rb
@@ -1,5 +1,7 @@
 class TaskiumController < ApplicationController
   def index
     @posts = Post.all
+    @categories = Category.all
+    @tags = Tag.all
   end
 end
--- taskium/app/models/post.rb
+++ taskium/app/models/post.rb
@@ -1,2 +1,4 @@
 class Post < ActiveRecord::Base
+  belongs_to :category
+  has_and_belongs_to_many :tags
 end
