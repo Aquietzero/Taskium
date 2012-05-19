aa6a206d68675c9f3d954361e131ec6292664d49
Yunhao Zhao <zhaoyunhaosss@gmail.com>
2012-05-19 11:01:37 +0800

    fix: bug in user authentication

--- taskium/app/controllers/application_controller.rb
+++ taskium/app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_filter :authorize
+  before_filter :set_user
   protect_from_forgery
 
   protected
@@ -7,5 +8,11 @@ class ApplicationController < ActionController::Base
     unless User.find(session[:user_id])
       redirect_to login_url, :notice => 'Please login first!'
     end 
+  end
+
+  def set_user
+    if session[:user_id]
+      @user = User.find(session[:user_id])
+    end
   end
 end
